1cc9f71e2a5772e2bc4a960748202320ceaa8d61
Michał Korzeniewski <michal.korzeniewskiii@gmail.com>
2024-12-01 16:12:00 +0100

    Create healthcheck_test.go

---
+++ routes/tests/healthcheck_test.go
@@ -0,0 +1,43 @@
+package tests
+
+import (
+	"encoding/json"
+	"fmt"
+	"io-project-api/internal/models"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"testing"
+)
+
+func TestRegisterAcademicTitle(t *testing.T) {
+
+	url := fmt.Sprintf("https://localhost:8000/healthcheck")
+
+	// Wykonaj zapytanie GET
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("Błąd podczas wysyłania zapytania: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Sprawdź, czy zapytanie zakończyło się sukcesem
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("Otrzymano błąd: %s", resp.Status)
+	}
+
+	// Wczytaj odpowiedź
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Błąd podczas odczytywania odpowiedzi: %v", err)
+	}
+
+	// Rozpakuj JSON do struktury
+	var result []models.Scientist
+
+	if err := json.Unmarshal(body, &result); err != nil {
+		log.Fatalf("Błąd podczas parsowania JSON: %v", err)
+	}
+
+	fmt.Printf("Academic title dla %s %s: %s\n", result[0].FirstName, result[0].LastName, result[0].AcademicTitle)
+}
